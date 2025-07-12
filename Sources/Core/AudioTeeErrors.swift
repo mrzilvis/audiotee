ee8968e2d6ea9843cd6e82a8230d6e0e5d95124a
Nick Payne <nick@kurai.co.uk>
2025-07-12 13:45:25 +0100

    no need for buffer overflow error

--- Sources/Core/AudioTeeErrors.swift
+++ Sources/Core/AudioTeeErrors.swift
@@ -8,7 +8,6 @@ public enum AudioTeeError: Error {
   case aggregateDeviceCreationFailed(OSStatus)
   case tapAssignmentFailed(OSStatus)
   case pidTranslationFailed([Int32])
-  case bufferOverflow(requested: Int, available: Int)
 }
 
 // MARK: - Audio Format Conversion Errors
